ac628fe1aa8d7fdeddff643f4badd35b28f0348b
Carmo-sousa <carmosousa20@outlook.com>
2021-06-27 08:10:32 -0400

    Fim da primeira parte

---
+++ 01-modelo-dados/1-1-exemplo.py
@@ -0,0 +1,29 @@
+""" um barralho com sequência de cartas """
+import collections
+from typing import NamedTuple
+
+Card: NamedTuple[str, str] = collections.namedtuple("Card", ["rank", "suit"])
+
+suit_values: dict[str, int] = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card: Card) -> int:
+    rank_value: int = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
+
+    def __init__(self) -> None:
+        self._cards: list[Card] = [
+            Card(rank, suit) for suit in self.suits
+            for rank in self.ranks
+        ]
+
+    def __len__(self) -> int:
+        """Retorna o tamanho do deck"""
+        return len(self._cards)
+    
+
+    def __getitem__(self, position: int) -> Card:
+        return self._cards[position]
--- 01-modelo-dados/README.md
+++ 01-modelo-dados/README.md
@@ -4,3 +4,7 @@
 designer de linguagens que poderiam ter criado belas linguagens em teoria, mas que
 ninguém jamais usaria, porém Guido é uma dessas raras pessoas capazes de criar uma linguagem apenas um pouco menos bela teoricamente, mas com á qual é um prazer escrever programas*
 -Jim Hugunin - criador do jython, cocriador do AspectJ, arquiteto do DLR do .net
+
+## Exemplos
+
+[1.1 Um baralho com uma sequência de cartas](./1-1-exemplo.py)
