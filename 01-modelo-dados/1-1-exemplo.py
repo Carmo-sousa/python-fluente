58ccf6fa3dab128351dfbf568621d219e313b452
Carmo-sousa <carmosousa20@outlook.com>
2021-06-27 09:36:25 -0400

    Adicionando o exemplo 1.2

--- 01-modelo-dados/1-1-exemplo.py
+++ 01-modelo-dados/1-1-exemplo.py
@@ -6,9 +6,11 @@ Card: NamedTuple[str, str] = collections.namedtuple("Card", ["rank", "suit"])
 
 suit_values: dict[str, int] = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
+
 def spades_high(card: Card) -> int:
     rank_value: int = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
+
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list("JQKA")
@@ -16,14 +18,12 @@ class FrenchDeck:
 
     def __init__(self) -> None:
         self._cards: list[Card] = [
-            Card(rank, suit) for suit in self.suits
-            for rank in self.ranks
+            Card(rank, suit) for suit in self.suits for rank in self.ranks
         ]
 
     def __len__(self) -> int:
         """Retorna o tamanho do deck"""
         return len(self._cards)
-    
 
     def __getitem__(self, position: int) -> Card:
         return self._cards[position]
